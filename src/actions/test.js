c4804050a0f46e0cb7669c4fc3cd567013c91353
gergokutu <gergo.kutu@gmail.com>
2019-07-19 13:18:10 +0200

    action creators

---
+++ src/actions/test.js
@@ -0,0 +1,20 @@
+// export function helloWorld(firstName, lastName) {
+//   return {
+//     type: 'NEW_HELLO_WORLD',
+//     payload: {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+//   }
+// }
+
+export function helloWorld(firstName, lastName) {
+  return {
+    type: 'NEW_HELLO_WORLD',
+    payload: {
+      // with Object Literal Property Value Shorthand
+      firstName,
+      lastName
+    }
+  }
+}
--- src/components/AlbumsListContainer.js
+++ src/components/AlbumsListContainer.js
@@ -2,12 +2,30 @@ import * as React from 'react'
 import * as request from 'superagent'
 import AlbumsList from './AlbumsList'
 
-export default class AlbumsListContainer extends React.Component {
+import { connect } from 'react-redux'
+import { helloWorld } from '../actions/test'
+
+class AlbumsListContainer extends React.Component {
   state = {}
 
   componentDidMount() {
     request('https://jsonplaceholder.typicode.com/albums')
       .then(response => this.setState({ albums: response.body }))
+
+      // called from test.js
+      // helloWorld has to be called as a function
+      // better to put it into a separate variable
+      // and dispatch that
+      // in this case params shoould be strings...
+      // ...you do not have to declare them
+      // const action = helloWorld('anything', 'other anything')
+      // this.props.dispatch(action)
+      
+      // dispatch is not necessary like above
+      // If we bind action creators using connect...
+      // this.props.dispatch is no longer available.
+      // to use this we can also correct export at the bottom
+      this.props.helloWorld('Alice', 'Seriously Alice')
   }
 
   render() {
@@ -15,3 +33,7 @@ export default class AlbumsListContainer extends React.Component {
     return <AlbumsList albums={this.state.albums} />
   }
 }
+
+// export default connect()(AlbumsListContainer)
+// binding action creators with connect
+export default connect(null, { helloWorld: helloWorld })(AlbumsListContainer)
